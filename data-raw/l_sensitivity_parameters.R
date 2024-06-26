8072b76301e1b3a3b4c240d3d81380f145a542e5
Khader Habash <khaderhabash@gmail.com>
2024-06-26 10:28:57 +0300

    making the list dynamic

--- data-raw/l_sensitivity_parameters.R
+++ data-raw/l_sensitivity_parameters.R
@@ -3,135 +3,219 @@
 # Define sensitivity parameters from the Chinese paper (Add reference)
 l_sensitivity_parameters <- list(
   body_weight = list(
-    "mean" = 15,
-    "lower_ci" = 10,
-    "upper_ci" = 20,
+    "mean" = l_psm_parameters$body_weight,
+    "lower_ci" = l_psm_parameters$body_weight*0.667,
+    "upper_ci" = l_psm_parameters$body_weight*1.333,
     "distribution" = "normal"
   ),
   GD2_unit_price = list(
-    "mean" = 8790.26,
-    "lower_ci" = 7032.21,
-    "upper_ci" = 10548.31,
+    "mean" = l_psm_parameters$GD2_unit_price,
+    "lower_ci" = l_psm_parameters$GD2_unit_price*0.8,
+    "upper_ci" = l_psm_parameters$GD2_unit_price*1.2,
     "distribution" = "gamma"
   ),
   TT_unit_price = list(
-    "mean" = 0.24,
-    "lower_ci" = 0.19,
-    "upper_ci" = 0.29,
+    "mean" = l_psm_parameters$TT_unit_price,
+    "lower_ci" = l_psm_parameters$TT_unit_price*0.8,
+    "upper_ci" = l_psm_parameters$TT_unit_price*1.2,
     "distribution" = "gamma"
   ),
+  prob_anemia = list(
+    "mean" = l_psm_parameters$prob_anemia,
+    "lower_ci" = l_psm_parameters$prob_anemia*0.8,
+    "upper_ci" = l_psm_parameters$prob_anemia*1.2,
+    "distribution" = "beta"
+  ),
+  prob_leukopenia = list(
+    "mean" = l_psm_parameters$prob_leukopenia,
+    "lower_ci" = l_psm_parameters$prob_leukopenia*0.8,
+    "upper_ci" = l_psm_parameters$prob_leukopenia*1.2,
+    "distribution" = "beta"
+  ),
+  prob_neutropenia = list(
+    "mean" = l_psm_parameters$prob_neutropenia,
+    "lower_ci" = l_psm_parameters$prob_neutropenia*0.8,
+    "upper_ci" = l_psm_parameters$prob_neutropenia*1.2,
+    "distribution" = "beta"
+  ),
+  prob_thrombocytopenia = list(
+    "mean" = l_psm_parameters$prob_thrombocytopenia,
+    "lower_ci" = l_psm_parameters$prob_thrombocytopenia*0.8,
+    "upper_ci" = l_psm_parameters$prob_thrombocytopenia*1.2,
+    "distribution" = "beta"
+  ),
   prob_fever = list(
-    "mean" = 0.18,
-    "lower_ci" = 0.14,
-    "upper_ci" = 0.22,
+    "mean" = l_psm_parameters$prob_fever,
+    "lower_ci" = l_psm_parameters$prob_fever*0.8,
+    "upper_ci" = l_psm_parameters$prob_fever*1.2,
+    "distribution" = "beta"
+  ),
+  prob_urticaria = list(
+    "mean" = l_psm_parameters$prob_urticaria,
+    "lower_ci" = l_psm_parameters$prob_urticaria*0.8,
+    "upper_ci" = l_psm_parameters$prob_urticaria*1.2,
+    "distribution" = "beta"
+  ),
+  prob_immunotherapy_related_pain = list(
+    "mean" = l_psm_parameters$prob_immunotherapy_related_pain,
+    "lower_ci" = l_psm_parameters$prob_immunotherapy_related_pain*0.8,
+    "upper_ci" = l_psm_parameters$prob_immunotherapy_related_pain*1.2,
     "distribution" = "beta"
   ),
   prob_diarrhea = list(
-    "mean" = 0.07,
-    "lower_ci" = 0.056,
-    "upper_ci" = 0.084,
+    "mean" = l_psm_parameters$prob_fever,
+    "lower_ci" = l_psm_parameters$prob_fever*0.8,
+    "upper_ci" = l_psm_parameters$prob_fever*1.2,
+    "distribution" = "beta"
+  ),
+  prob_hypotension = list(
+    "mean" = l_psm_parameters$prob_hypotension,
+    "lower_ci" = l_psm_parameters$prob_hypotension*0.8,
+    "upper_ci" = l_psm_parameters$prob_hypotension*1.2,
     "distribution" = "beta"
   ),
   prob_vomiting = list(
-    "mean" = 0.06,
-    "lower_ci" = 0.048,
-    "upper_ci" = 0.072,
+    "mean" = l_psm_parameters$prob_vomiting,
+    "lower_ci" = l_psm_parameters$prob_vomiting*0.8,
+    "upper_ci" = l_psm_parameters$prob_vomiting*1.2,
     "distribution" = "beta"
   ),
   prob_infection = list(
-    "mean" = 0.21,
-    "lower_ci" = 0.16,
-    "upper_ci" = 0.25,
+    "mean" = l_psm_parameters$prob_infection,
+    "lower_ci" = l_psm_parameters$prob_infection*0.8,
+    "upper_ci" = l_psm_parameters$prob_infection*1.2,
     "distribution" = "beta"
   ),
   prob_hypersensitivity_reaction = list(
-    "mean" = 0.11,
-    "lower_ci" = 0.088,
-    "upper_ci" = 0.13,
+    "mean" = l_psm_parameters$prob_hypersensitivity_reaction,
+    "lower_ci" = l_psm_parameters$prob_hypersensitivity_reaction*0.8,
+    "upper_ci" = l_psm_parameters$prob_hypersensitivity_reaction*1.2,
     "distribution" = "beta"
   ),
   prob_capillary_leak = list(
-    "mean" = 0.06,
-    "lower_ci" = 0.04,
-    "upper_ci" = 0.08,
+    "mean" = l_psm_parameters$prob_capillary_leak,
+    "lower_ci" = l_psm_parameters$prob_capillary_leak*0.8,
+    "upper_ci" = l_psm_parameters$prob_capillary_leak*1.2,
     "distribution" = "beta"
   ),
+  cost_anemia = list(
+    "mean" = l_psm_parameters$cost_anemia,
+    "lower_ci" = l_psm_parameters$cost_anemia*0.8,
+    "upper_ci" = l_psm_parameters$cost_anemia*1.2,
+    "distribution" = "gamma"
+  ),
+  cost_leukopenia= list(
+    "mean" = l_psm_parameters$cost_leukopenia,
+    "lower_ci" = l_psm_parameters$cost_leukopenia*0.8,
+    "upper_ci" = l_psm_parameters$cost_leukopenia*1.2,
+    "distribution" = "gamma"
+  ),
+  cost_neutropenia = list(
+    "mean" = l_psm_parameters$cost_neutropenia,
+    "lower_ci" = l_psm_parameters$cost_neutropenia*0.8,
+    "upper_ci" = l_psm_parameters$cost_neutropenia*1.2,
+    "distribution" = "gamma"
+  ),
+  cost_thrombocytopenia = list(
+    "mean" = l_psm_parameters$cost_thrombocytopenia,
+    "lower_ci" = l_psm_parameters$cost_thrombocytopenia*0.8,
+    "upper_ci" = l_psm_parameters$cost_thrombocytopenia*1.2,
+    "distribution" = "gamma"
+  ),
   cost_fever = list(
-    "mean" = 1.98,
-    "lower_ci" = 1.58,
-    "upper_ci" = 2.38,
+    "mean" = l_psm_parameters$cost_fever,
+    "lower_ci" = l_psm_parameters$cost_fever*0.8,
+    "upper_ci" = l_psm_parameters$cost_fever*1.2,
+    "distribution" = "gamma"
+  ),
+  cost_urticaria = list(
+    "mean" = l_psm_parameters$cost_urticaria,
+    "lower_ci" = l_psm_parameters$cost_urticaria*0.8,
+    "upper_ci" = l_psm_parameters$cost_urticaria*1.2,
     "distribution" = "gamma"
   ),
   cost_diarrhea = list(
-    "mean" = 3.2,
-    "lower_ci" = 2.56,
-    "upper_ci" = 3.84,
+    "mean" = l_psm_parameters$cost_diarrhea,
+    "lower_ci" = l_psm_parameters$cost_diarrhea*0.8,
+    "upper_ci" = l_psm_parameters$cost_diarrhea*1.2,
+    "distribution" = "gamma"
+  ),
+  cost_immunotherapy_related_pain = list(
+    "mean" = l_psm_parameters$cost_immunotherapy_related_pain,
+    "lower_ci" = l_psm_parameters$cost_immunotherapy_related_pain*0.8,
+    "upper_ci" = l_psm_parameters$cost_immunotherapy_related_pain*1.2,
     "distribution" = "gamma"
   ),
   cost_vomiting = list(
-    "mean" = 8.45,
-    "lower_ci" = 6.76,
-    "upper_ci" = 10.14,
+    "mean" = l_psm_parameters$cost_vomiting,
+    "lower_ci" = l_psm_parameters$cost_vomiting*0.8,
+    "upper_ci" = l_psm_parameters$cost_vomiting*1.2,
+    "distribution" = "gamma"
+  ),
+  cost_hypotension  = list(
+    "mean" = l_psm_parameters$cost_hypotension ,
+    "lower_ci" = l_psm_parameters$cost_hypotension *0.8,
+    "upper_ci" = l_psm_parameters$cost_hypotension *1.2,
     "distribution" = "gamma"
   ),
   cost_infection = list(
-    "mean" = 222.33,
-    "lower_ci" = 177.86,
-    "upper_ci" = 266.80,
+    "mean" = l_psm_parameters$cost_infection,
+    "lower_ci" = l_psm_parameters$cost_infection*0.8,
+    "upper_ci" = l_psm_parameters$cost_infection*1.2,
     "distribution" = "gamma"
   ),
   cost_hypersensitivity_reaction = list(
-    "mean" = 4.13,
-    "lower_ci" = 3.30,
-    "upper_ci" = 4.96,
+    "mean" = l_psm_parameters$cost_hypersensitivity_reaction,
+    "lower_ci" = l_psm_parameters$cost_hypersensitivity_reaction*0.8,
+    "upper_ci" = l_psm_parameters$cost_hypersensitivity_reaction*1.2,
     "distribution" = "gamma"
   ),
   cost_capillary_leak = list(
-    "mean" = 307.37,
-    "lower_ci" = 245.90,
-    "upper_ci" = 368.84,
+    "mean" = l_psm_parameters$cost_capillary_leak,
+    "lower_ci" = l_psm_parameters$cost_capillary_leak*0.8,
+    "upper_ci" = l_psm_parameters$cost_capillary_leak*1.2,
     "distribution" = "gamma"
   ),
   cost_pain = list(
-    "mean" = 6.36,
-    "lower_ci" = 5.09,
-    "upper_ci" = 7.63,
+    "mean" = l_psm_parameters$cost_pain,
+    "lower_ci" = l_psm_parameters$cost_pain*0.8,
+    "upper_ci" = l_psm_parameters$cost_pain*1.2,
     "distribution" = "gamma"
   ),
   cost_laboratory = list(
-    "mean" = 141.45,
-    "lower_ci" = 113.16,
-    "upper_ci" = 169.74,
+    "mean" = l_psm_parameters$cost_laboratory,
+    "lower_ci" = l_psm_parameters$cost_laboratory*0.8,
+    "upper_ci" = l_psm_parameters$cost_laboratory*1.2,
     "distribution" = "gamma"
   ),
   cost_hospitalisation = list(
-    "mean" = 3763.62,
-    "lower_ci" = 3010.90,
-    "upper_ci" = 4516.34,
+    "mean" = l_psm_parameters$cost_hospitalisation,
+    "lower_ci" = l_psm_parameters$cost_hospitalisation*0.8,
+    "upper_ci" = l_psm_parameters$cost_hospitalisation*1.2,
     "distribution" = "gamma"
   ),
   Temo_unit_price = list(
-    "mean" = 15.34,
-    "lower_ci" = 12.27,
-    "upper_ci" = 18.41,
+    "mean" = l_psm_parameters$Temo_unit_price,
+    "lower_ci" = l_psm_parameters$Temo_unit_price*0.95,
+    "upper_ci" = l_psm_parameters$Temo_unit_price*1.05,
     "distribution" = "gamma"
   ),
   Iri_unit_price = list(
-    "mean" = 4.4,
-    "lower_ci" = 3.52,
-    "upper_ci" = 5.28,
+    "mean" = l_psm_parameters$Iri_unit_price,
+    "lower_ci" = l_psm_parameters$Iri_unit_price*0.8,
+    "upper_ci" = l_psm_parameters$Iri_unit_price*1.2,
     "distribution" = "gamma"
   ),
   u_EFS = list(
-    "mean" = 0.84,
-    "lower_ci" = 0.76,
-    "upper_ci" = 0.92,
+    "mean" = l_psm_parameters$u_EFS,
+    "lower_ci" = l_psm_parameters$u_EFS*0.9,
+    "upper_ci" = l_psm_parameters$u_EFS*1.1,
     "distribution" = "beta"
   ),
   u_PPS = list(
-    "mean" = 0.56,
-    "lower_ci" = 0.50,
-    "upper_ci" = 0.62,
+    "mean" = l_psm_parameters$u_PPS,
+    "lower_ci" = l_psm_parameters$u_PPS*0.9,
+    "upper_ci" = l_psm_parameters$u_PPS*1.1,
     "distribution" = "beta"
   )
 )
