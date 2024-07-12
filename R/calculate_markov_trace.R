f43d75248eca41578ab57fa004d07abfb1656ae3
Wael Mohammed <wmamohammed1@sheffield.ac.uk>
2024-07-12 18:53:33 +0100

    adjusted check precision

--- R/calculate_markov_trace.R
+++ R/calculate_markov_trace.R
@@ -168,7 +168,8 @@ calculate_markov_trace <- function(
   # Sanity check:
   stopifnot(
     "Markov trace does not sum up to 1." =
-      all(rowSums(df_markov_trace[, c("EFS", "PPS", "D")]) == 1))
+      all(rowSums(df_markov_trace[, c("EFS", "PPS", "D")])  == 1.000000)
+  )
 
   return(df_markov_trace)
 }
